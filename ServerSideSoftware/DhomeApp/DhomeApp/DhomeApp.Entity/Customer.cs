d6255edb1c24cb32d141b1ff58bd74d3e8ef8d19
Gianella <gia.ne12@hotmail.com>
2021-05-05 21:17:40 -0500

    Uploading Project

---
+++ ServerSideSoftware/DhomeApp/DhomeApp/DhomeApp.Entity/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DhomeApp.Entity
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Todo cliente debe registrar un nombre")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "El nombre del usuario debe tener entre 5 a 50 caracteres.")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Todo cliente debe registrar un apellido")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "El apellido del usuario debe tener entre 5 a 50 caracteres.")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Todo cliente debe registrar un DNI")]
+        [StringLength(8, ErrorMessage = "El DNI debe tener un máximo de 8 dígitos.")]
+        public string Dni { get; set; }
+
+        [Required(ErrorMessage = "Todo cliente debe registrar un email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Todo cliente debe registrar una contraseña")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "La contraseña debe tener entre 8 a 20 caracteres.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Todo cliente debe registrar una ciudad")]
+        public string City { get; set; }
+
+    }
+}
---
+++ ServerSideSoftware/DhomeApp/DhomeApp/DhomeApp.Web/Models/PutEmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DhomeApp.Web.Models
+{
+    public class PutEmployeeModel
+    {
+        public int EmployeeId { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Dni { get; set; }
+    }
+}
